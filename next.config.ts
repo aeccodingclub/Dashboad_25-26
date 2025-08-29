e40fdba989f8b12c50e02e76740a22613daa6ff0
poran-dip <porandip0@gmail.com>
2025-08-30 00:16:50 +0530

    GH Pages fix

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: isGitHubPages ? '/Dashboard_25-26' : '',
+  assetPrefix: isGitHubPages ? '/Dashboard_25-26/' : '/',
+  trailingSlash: true,
   outputFileTracingExcludes: {
     '/api/*': ['src/app/api/**/*'],
   }
